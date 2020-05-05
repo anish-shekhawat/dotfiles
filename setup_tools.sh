8b7c7b7db2f0eca93e0f21cc6a3b21f6fc55fc80
Anish Shekhawat <anish.s.shekhawat@gmail.com>
2020-05-04 20:07:16 -0500

    Fix spaceship-prompt script

--- setup_tools.sh
+++ setup_tools.sh
@@ -4,15 +4,12 @@ mkdir -p $HOME/tools
 mkdir -p $HOME/.zsh/zfunctions
 
 # setup zsh plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zfunctions/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-history-substring-search ~/.zsh/zfunctions/zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zfunctions/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.zsh/zfunctions/zsh-history-substring-search
 
 # setup zsh prompt
-git clone https://github.com/denysdovhan/spaceship-prompt.git $HOME/tools
-ln -sf "$HOME/tools/spaceship-prompt/spaceship.zsh" "$HOME/.zsh/zfunctions/prompt_spaceship_setup"
-fpath=( "$HOME/.zsh/zfunctions" $fpath )
-autoload -U promptinit; promptinit
-prompt spaceship
+git clone https://github.com/denysdovhan/spaceship-prompt.git $HOME/.zsh/zfunctions/spaceship-prompt
+ln -sf "$HOME/.zsh/zfunctions/spaceship-prompt/spaceship.zsh" "$HOME/.zsh/zfunctions/prompt_spaceship_setup"
 
 SPACESHIP_GIT_STATUS_SHOW=false
 SPACESHIP_EXIT_CODE_SHOW=true
@@ -30,3 +27,5 @@ SPACESHIP_PROMPT_ORDER=(
 # Copy .zsh files
 cp .zshrc $HOME/.zshrc
 cp .zshalias $HOME/.zsh/zshalias
+
+source $HOME/.zshrc
